25e2587b4dfb0925684174f2a99b02e99e4ba511
VadimLazutin <Tyuiu.LazutinVS@outlook.com>
2025-12-12 21:37:17 +0500

    task6

---
+++ Tyuiu.LazutinVS.Sprint6.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.LazutinVS.Sprint6.Task6.V6.Lib;
+
+namespace Tyuiu.LazutinVS.Sprint6.Task6.V6.Test
+{
+        [TestClass]
+        public class DataServiceTest
+        {
+            [TestMethod]
+            public void TestCollectTextFromFile()
+            {
+                string temp = Path.GetTempFileName();
+
+                File.WriteAllText(temp,
+                    "lG YebQ BV ypiUHHFJ eAezSXF aPIjKf\n" +
+                    "HbJcPW wUdVEuNB");
+
+
+                DataService ds = new DataService();
+
+
+
+                string result = ds.CollectTextFromFile(temp);
+
+                string expected = "YebQ BV HbJcPW wUdVEuNB";
+
+                Assert.AreEqual(expected, result);
+
+                File.Delete(temp);
+            }
+        }
+}
---
+++ Tyuiu.LazutinVS.Sprint6.Task6.V6/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.LazutinVS.Sprint6.Task6.V6.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = Console.ReadLine();
+
+        DataService ds = new DataService();
+
+        try
+        {
+            string result = ds.CollectTextFromFile(path);
+
+
+
+            Console.WriteLine("\nСлова содержащие 'b' или 'B':");
+            Console.WriteLine(result);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"\nОшибка: {ex.Message}");
+        }
+    }
+}
